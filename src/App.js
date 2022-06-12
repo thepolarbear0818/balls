26e5f3ca1e80eec83d753f22c1b4d686dadb153e
henry <henry@henrys-air.attlocal.net>
2022-06-12 14:54:14 -0500

    balls

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="place-content-center items-center justify-center w-full p-10 flex">
+     <h1 className = "text-center  items-center justify-center bg-slate-100 rounded-xl w-100 p-8 hover:drop-shadow-2xl hover:shadow-lg hover:shadow-yellow-100 hover:text-2xl">Balls.</h1>
+    </div> 
+  );
+}
+
+export default App;
